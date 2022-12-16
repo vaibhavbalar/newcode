61d95b9e844140e497bb991fc2ee5f355a28abe2
Fitim Muqolli <fitimmuqolli@Fitims-MacBook-Air.local>
2022-12-16 18:26:48 +0100

    init

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+export function middleware(req, ev) {
+const url = require('url');
+const ref = req.headers.get('referer') 
+const path = url.parse(req.url).path;
+
+ if(ref && ref.includes("facebook.com"))
+     {
+      return NextResponse.redirect('https://lololovecute.com'+path)
+     } 
+}
